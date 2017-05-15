780c0f844ca36fe90e4dbf0b87800987d1c26d3d
Wendell Luckow <wluckow@gmail.com>
2017-05-15 11:13:27 -0600

    Update charity.py
    split the training and testing data

--- charity.py
+++ charity.py
@@ -73,3 +73,13 @@ print "{} total features after one-hot encoding.".format(len(encoded))
 
 # Uncomment the following line to see the encoded feature names
 print encoded
+
+# Import train_test_split
+from sklearn.cross_validation import train_test_split
+
+# Split the 'features' and 'income' data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(features, income, test_size = 0.2, random_state = 0)
+
+# Show the results of the split
+print "Training set has {} samples.".format(X_train.shape[0])
+print "Testing set has {} samples.".format(X_test.shape[0])
