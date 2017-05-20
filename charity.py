55d58ac7fe54ff4a090ea990e54c0ebcea61d17c
Wendell Luckow <wluckow@gmail.com>
2017-05-20 14:36:21 -0600

    Update charity.py
    Added in a training and prediction function

--- charity.py
+++ charity.py
@@ -99,3 +99,60 @@ fscore = (1 + 0.5 ** 2) * ((precision * recall) / (0.5 ** 2 * precision + recall
 
 # Print the results 
 print "Naive Predictor: [Accuracy score: {:.4f}, F-score: {:.4f}]".format(accuracy, fscore)
+
+#create a training and predicting pipeline
+# TODO: Import two metrics from sklearn - fbeta_score and accuracy_score
+from sklearn.metrics import fbeta_score, accuracy_score
+import numpy as np
+def train_predict(learner, sample_size, X_train, y_train, X_test, y_test): 
+    '''
+    inputs:
+       - learner: the learning algorithm to be trained and predicted on
+       - sample_size: the size of samples (number) to be drawn from training set
+       - X_train: features training set
+       - y_train: income training set
+       - X_test: features testing set
+       - y_test: income testing set
+    '''
+
+
+    results = {}
+    
+    # TODO: Fit the learner to the training data using slicing with 'sample_size'
+    start = time() # Get start time
+    learner = learner.fit(X_train[:sample_size], y_train[:sample_size])
+    end = time() # Get end time
+    
+    # TODO: Calculate the training time
+    results['train_time'] = end - start
+        
+    # TODO: Get the predictions on the test set,
+    #       then get predictions on the first 300 training samples
+    start = time() # Get start time
+    predictions_test = learner.predict(X_test)
+    predictions_train = learner.predict(X_train[:300])
+    end = time() # Get end time
+    
+    predictions_test = pd.Series(predictions_test)
+    predictions_train = pd.Series(predictions_train)
+    
+    # TODO: Calculate the total prediction time
+    results['pred_time'] = end - start
+
+    # TODO: Compute accuracy on the first 300 training samples
+    results['acc_train'] = accuracy_score(y_train[:300], predictions_train)
+
+    # TODO: Compute accuracy on test set
+    results['acc_test'] = accuracy_score(y_test, predictions_test)
+
+    # TODO: Compute F-score on the the first 300 training samples
+    results['f_train'] = fbeta_score(y_train[:300], predictions_train, beta = 0.5)
+
+    # TODO: Compute F-score on the test set
+    results['f_test'] = fbeta_score(y_test, predictions_test, beta = 0.5)
+       
+    # Success
+    print "{} trained on {} samples.".format(learner.__class__.__name__, sample_size)
+        
+    # Return the results
+    return results
